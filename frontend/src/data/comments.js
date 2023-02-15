45c85b0c3724ecb704ea1a547f2cac8e281c763f
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-02-15 22:38:05 +0100

    add comments

--- frontend/src/data/comments.js
+++ frontend/src/data/comments.js
@@ -4,7 +4,7 @@ export const getCommentsData = async () => {
       _id: "10",
       user: {
         _id: "a",
-        name: "Mohammad Rezaii",
+        name: "Thierno",
       },
       desc: "it was a nice post, Thank you!",
       post: "1",
@@ -16,7 +16,7 @@ export const getCommentsData = async () => {
       _id: "11",
       user: {
         _id: "b",
-        name: "Paul M. Williams",
+        name: "Barry",
       },
       desc: "a reply for Mohammad",
       post: "1",
@@ -28,7 +28,7 @@ export const getCommentsData = async () => {
       _id: "12",
       user: {
         _id: "b",
-        name: "Paul M. Williams",
+        name: "Barry",
       },
       desc: "keep it up bro <3",
       post: "1",
@@ -40,7 +40,7 @@ export const getCommentsData = async () => {
       _id: "13",
       user: {
         _id: "c",
-        name: "Jessica C. Stephens",
+        name: "Barry",
       },
       desc: "I'm always interested in your content :)",
       post: "1",
