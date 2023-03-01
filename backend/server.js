326b58fe6e0dd8df45a75be3937faeb183feab70
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-03-01 10:55:48 +0100

    add register

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import userRoutes from "./routes/userRoutes"
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
