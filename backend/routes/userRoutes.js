e6350e650bd5283faab987f15038ea06505b05fb
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-03-05 22:51:50 +0100

    add login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/userController";
+import { loginUser, registerUser, userProfile } from "../controllers/userController";
+import { authGuard } from "../middleware/authMiddleware";
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", authGuard, userProfile);
 
 export default router;
