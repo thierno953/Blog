d734b37c360ddf7e3fb7a2dc76d35b8c3a2f772d
Thierno <77080504+thierno953@users.noreply.github.com>
2023-03-24 10:04:33 +0100

    LoginPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/register/LoginPage";
 import RegisterPage from "./pages/register/RegisterPage";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route index path="/" element={<HomePage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
