19bbeef74897ae5fb057076e2e77c29c789d92a8
Thierno <77080504+thierno953@users.noreply.github.com>
2023-03-29 16:52:56 +0200

    CTA

--- frontend/src/pages/home/container/CTA.jsx
+++ frontend/src/pages/home/container/CTA.jsx
@@ -4,22 +4,6 @@ import { images } from "../../../constants";
 const CTA = () => {
   return (
     <>
-      {/* <svg
-        className="w-full h-auto max-h-40 translate-y-[1px]"
-        preserveAspectRatio="none"
-        viewBox="0 0 2160 263"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          id="Wave"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M2160 262.5H0V0C360 80 720 120 1080 120C1440 120 1800 80 2160 0V262.5Z"
-          fill="#0D2436"
-        />
-      </svg> */}
-
       <section className="relative bg-dark-hard px-5">
         <div className="container grid grid-cols-12 mx-auto py-10 md:pb-20 lg:place-items-center">
           <div className="col-span-12 lg:col-span-6">
