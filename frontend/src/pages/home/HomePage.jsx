4b1fbe3c6409bebf76f13f4c04f334438e43085e
Thierno <77080504+thierno953@users.noreply.github.com>
2023-03-29 16:51:22 +0200

    CTA

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MainLayout from "../../components/MainLayout";
 import Articles from "./container/Articles";
+import CTA from "./container/CTA";
 import Hero from "./container/Hero";
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
     <MainLayout>
       <Hero />
       <Articles />
+      <CTA />
     </MainLayout>
   );
 };
