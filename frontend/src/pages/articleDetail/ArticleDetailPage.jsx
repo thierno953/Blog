c19ba2306c939244a3063f342fa7b65ec448f157
Thierno <77080504+thierno953@users.noreply.github.com>
2023-03-30 11:43:41 +0200

    Social

--- frontend/src/pages/articleDetail/ArticleDetailPage.jsx
+++ frontend/src/pages/articleDetail/ArticleDetailPage.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import BreadCrumbs from "../../components/BreadCrumbs";
 import CommentsContainer from "../../components/Comments/CommentsContainer";
 import MainLayout from "../../components/MainLayout";
+import SocialShareButtons from "../../components/SocialShareButtons";
 import { images } from "../../constants";
 import SuggestedPosts from "./container/SuggestedPosts";
 
@@ -89,6 +90,19 @@ const ArticleDetailPage = () => {
             tags={tagsData}
             className="mt-8 lg:mt-0 lg:max-w-xs"
           />
+          <div className="mt-7">
+            <h2 className="mb-4 font-medium font-roboto text-dark-hard md:text-xl">
+              Share on:
+            </h2>
+            <SocialShareButtons
+              url={encodeURI(
+                "https://moonfo.com/post/client-side-and-server-side-explanation"
+              )}
+              title={encodeURIComponent(
+                "Client-side and Server-side explanation"
+              )}
+            />
+          </div>
         </div>
       </section>
     </MainLayout>
